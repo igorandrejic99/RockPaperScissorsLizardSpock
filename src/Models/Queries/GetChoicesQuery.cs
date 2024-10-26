1ca5e12b98571e857946ef4a01bbd98b5e6889b5
igorandrejic <igorandrejic99@gmail.com>
2024-10-27 00:19:40 +0100

    added xml comments

--- src/Models/Queries/GetChoicesQuery.cs
+++ src/Models/Queries/GetChoicesQuery.cs
@@ -1,5 +1,11 @@
 namespace RockPaperScissorsGame.Models.Queries
 {
+    /// <summary>
+    /// Represents a query to retrieve the list of available choices in the Rock-Paper-Scissors-Lizard-Spock game.
+    /// </summary>
+    /// <remarks>
+    /// This query does not require any parameters and is used to fetch the available game choices.
+    /// </remarks>
     public class GetChoicesQuery
     {
         // No parameters needed for fetching choices
